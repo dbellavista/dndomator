1d202157977428164123301a98c47b64821d6a9b
Daniele Bellavista <daniele.bellavista@gmail.com>
2013-03-11 17:04:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# D&D'o'mator, the d&d encounter and more automator!
+
+To be developed :)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dndomator::Application.config.secret_token = 'dda0a4ae4bff3bd78aa0bec749cac5d6ae0daa9e27e60f562d52f5526584ec695fe4e2da735a43b5622147767f431c98011116433fa4853b1af51e3f539b28db'
